34fe5f64dee74704ca54ba072b3aaccad0b9781c
Augusto1404 <augustopelis4@gmail.com>
2024-11-15 10:33:13 -0500

    delete entrypoint :)

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Crea el archivo de configuración en el directorio público
-echo "window.API_BASE_URL='${API_BASE_URL}';" > /app/config.js
-
-# Ejecuta el comando especificado en el CMD
-exec "$@"
